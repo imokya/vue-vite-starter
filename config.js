9e48b625abf1f15648e7ccb3893dd6a225df0b87
876657294@qq.com <876657294@qq.com>
2023-06-11 17:44:47 +0800

    new commit

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
-const config = {
-  publicPath: '/'
+const devConfig = {
+  publicPath: '/',
 }
+
+const prodConfig = {
+  publicPath: '/',
+}
+
+const config = process.env.NODE_ENV === 'development' ? devConfig : prodConfig
 
 export default config
